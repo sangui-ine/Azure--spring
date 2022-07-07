684b664a71f5028bca43d75a1a9d9f99f0a4c44e
sonal <sonaliinindiaa@gmail.com>
2022-07-07 20:09:49 +0530

    my first commit

---
+++ src/main/java/com/azurespring/azure/spring/AzureSpringApplication.java
@@ -0,0 +1,13 @@
+package com.azurespring.azure.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azurespring/azure/spring/AzureSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azurespring.azure.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
