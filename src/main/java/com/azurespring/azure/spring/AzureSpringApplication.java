5a6b3ade76fb57fdb1a374d0698fed0cc90b27bb
sonal <sonaliinindiaa@gmail.com>
2022-07-08 09:45:15 +0530

    second commit

--- src/main/java/com/azurespring/azure/spring/AzureSpringApplication.java
+++ src/main/java/com/azurespring/azure/spring/AzureSpringApplication.java
@@ -2,9 +2,16 @@ package com.azurespring.azure.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AzureSpringApplication {
+	@GetMapping("/message")
+	public String message() {
+		return "Congrats ! your apppp is deployed!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AzureSpringApplication.class, args);
